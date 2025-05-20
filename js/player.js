6b847c9c3f8f1c02544ca47a3b580acb258006f6
K0meijiSatori <yukikaze_poi@outlook.com>
2025-05-20 19:00:38 +0800

    Update player.js

--- js/player.js
+++ js/player.js
@@ -2,16 +2,16 @@ document.addEventListener("DOMContentLoaded", function() {
   Amplitude.init({
     "songs": [
       {
-        "name": "Track One",
-        "artist": "Artist A",
-        "album": "Album X",
+        "name": "拼凑的革命",
+        "artist": "UnKnow",
+        "album": "Na/N",
         "url": "media/track1.mp3",
         "cover_art_url": "media/cover1.jpg"
       },
       {
-        "name": "Track Two",
-        "artist": "Artist B",
-        "album": "Album Y",
+        "name": "南京杀人太少，应在南京多杀",
+        "artist": "毛",
+        "album": "Na/N",
         "url": "media/track2.mp3",
         "cover_art_url": "media/cover2.jpg"
       },
